2c8b48c3de1674c21cb75a05c840e83981e7024d
Maths Physis <smathsphysis@gmail.com>
2021-08-27 22:21:17 -0400

    refactor: Extraído os controllers para arquivo separado

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const Koa = require('koa');
 
 const bodyParser = require('koa-bodyparser');
 
-const Router = require('koa-router');
+const router = require('./controllers');
 
 const koa = new Koa();
 
@@ -27,67 +27,14 @@ koa.use(async (ctx, next) => {
 
 koa.use(bodyParser());
 
-var router = new Router();
-
-let usersDB = [];
-
-//rota simples pra testar se o servidor está online
-router.get('/', async (ctx) => {
-  ctx.body = `Seu servidor esta rodando em http://localhost:${PORT}`; //http://localhost:3000/
-});
-
-//Uma rota de exemplo simples aqui.
-//As rotas devem ficar em arquivos separados, /src/controllers/userController.js por exemplo
-router.get('/users', async (ctx) => {
-    ctx.status = 200;
-    ctx.body = { total:usersDB.length, count: 0, rows: usersDB };
-});
-
-router.get('/users/:id', async (ctx) => {
-    const { id } = ctx.params;
-    const user = usersDB.filter((user) => user.nome === id);
-    
-    if(user.length === 0) {
-      ctx.throw(404, 'User not found');
-    }
-
-    ctx.response.status = 200;
-    ctx.body = user;
-});
-
-router.post('/users', async (ctx) => {
-  const user = { ...ctx.request.body };
-  usersDB.push(user);
-  ctx.response.status = 201;
-  ctx.body = user;
-});
-
-router.put('/users/:id', async (ctx) => {
-  const userToUpdate = { ...ctx.request.body };
-  usersDB[0] = userToUpdate;
-  ctx.response.status = 204;
-});
-
-router.patch('/users/:id', async (ctx) => {
-  const { op, path, value } = ctx.request.body;
-  const { id } = ctx.params;
-  const field = path.substring(1);
-  usersDB.filter((user) => user.nome === id)
-  .forEach((userToUpdate) => {
-    userToUpdate[field] = value;
-  });
-  ctx.response.status = 204;
-});
-
-router.delete('/users/:id', async (ctx) => {
-  const { id } = ctx.params;
-  usersDB.splice(usersDB.indexOf(id), 1);
-  ctx.response.status = 204;
-});
-
 koa
   .use(router.routes())
   .use(router.allowedMethods());
+
+koa.on('error', (err, ctx) => {
+  console.error(err);
+  console.error(ctx.res.error.text);
+});
 
 const server = koa.listen(PORT);
 
