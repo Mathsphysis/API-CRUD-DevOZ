771b6b7eddb8690d56de3a78685c0f1d2eabff81
Maths Physis <smathsphysis@gmail.com>
2021-08-29 19:47:13 -0400

    test: Testes unitários para camada de serviço

--- test/service.js
+++ test/service.js
@@ -20,7 +20,7 @@ const userRepository = new UserRepository();
 
 const userService = new UserService(userRepository);
 
-const raupp = { nome: "raupp", email: "jose.raupp@devoz.com.br", idade: 35 };
+const raupp = { nome: 'raupp', email: 'jose.raupp@devoz.com.br', idade: 35 };
 
 //Inicio dos testes
 
@@ -28,7 +28,7 @@ const raupp = { nome: "raupp", email: "jose.raupp@devoz.com.br", idade: 35 };
 describe('Testes unitários para service',  () => {
 
     const loadDB = async function (usersToLoad = usersPredefined) {
-        const users = [ ...usersToLoad];
+        const users = [ ...usersToLoad, raupp];
         await userRepository.loadDB(users);
     };
     
@@ -47,7 +47,7 @@ describe('Testes unitários para service',  () => {
             const user = await userService.save(raupp);
             expect(user.nome).to.be.equal('raupp');
             expect(user.idade).to.be.equal(35);
-            expect(user.email).to.be.equal("jose.raupp@devoz.com.br");
+            expect(user.email).to.be.equal('jose.raupp@devoz.com.br');
         });
         
         it('cria usuários predefinidos', async function () {
@@ -75,7 +75,7 @@ describe('Testes unitários para service',  () => {
                 await userService.save(invalidUser);
             } catch (err) {
                 expect(err).to.haveOwnProperty('invalidFields');
-                expect(err.invalidFields).to.be.equal('InvalidModelFieldsError');
+                expect(err.invalidFields.length).to.be.equal(2);
             }
         });
 
@@ -89,82 +89,66 @@ describe('Testes unitários para service',  () => {
         after( async () => await cleanup() );
         it('deveria ser uma lista com pelo menos 5 usuarios', async function () {
             const users = await userService.findAll();
-            expect(users).to.have.property(total);
-            expect(users.total).to.be.equal(5);
+            expect(users).to.have.property('count');
+            expect(users.count).to.be.equal(6);
         });
-        it('o usuario naoExiste não existe no sistema', function (done) {
-            chai.request(app)
-            .get(`${BASE_URL}/users/naoExiste`)
-            .end(function (err, res) {
-                expect(res.body.message).to.be.equal('Not Found: User with WHERE name=\'naoExiste\' not found'); 
-                expect(res).to.have.status(404);
-                done();
-            });
+        it('o usuario naoExiste não existe no sistema', async function () {
+            try {
+                await userService.findOneByName('naoExiste');
+            } catch (err) {
+                expect(err).to.have.property('type');
+                expect(err.type).to.be.equal('UserNotFoundError');
+            }
         });
     
-        it('o usuario raupp existe e é valido', function (done) {
-            chai.request(app)
-            .get(`${BASE_URL}/users/raupp`)
-            .end(function (err, res) {
-                expect(err).to.be.null;
-                expect(res).to.have.status(200);
-                expect(res.body).to.be.jsonSchema(userSchema);
-                done();
-            });
+        it('o usuario raupp existe e é valido', async function () {
+            const user = await userService.findOneByName('raupp');
+            expect(user).to.be.jsonSchema(userSchema);
         });
-        it('retorna os resultados com paginação', (done) => {
-            chai.request(app)
-            .get(`${BASE_URL}/users?page=2`)
-            .end(function (err, res) {
-                expect(err).to.be.null;
-                expect(res).to.have.status(200);
-                expect(res.body.currentPage).to.be.equal(2);
-                done();
-            });
+        it('retorna os resultados com paginação', async  () => {
+            const page = '1';
+            const users = await userService.findAll(page);
+            expect(users).to.have.property('count');
+            expect(users).to.have.property('totalPages');
+            expect(users).to.have.property('rows');
+            expect(users).to.have.property('currentPage');
         });
-        it('retorna os resultados com paginação com limite definido pelo cliente', (done) => {
-            const users = [ ...usersPredefined, raupp ];
-            const limit = 2;
-            const page = 1;
-            const total = users.length;
+        it('retorna os resultados com paginação com limite definido pelo cliente', async  () => {
+            const usersDB = [ ...usersPredefined, raupp ];
+            const queryLimit = '2';
+            const queryPage = '1';
+            const limit = +queryLimit;
+            const page = +queryPage;
+            const total = usersDB.length;
             const totalPages = Math.ceil(total / limit);
             const isLastPage = page === totalPages;
             const lastPageCount = total - (totalPages - 1) * limit;
             const count = isLastPage ? lastPageCount : limit;
-            chai.request(app)
-            .get(`${BASE_URL}/users?page=${page}&limit=${limit}`)
-            .end(function (err, res) {
-                expect(err).to.be.null;
-                expect(res).to.have.status(200);
-                expect(res.body.totalPages).to.be.equal(totalPages);
-                expect(res.body.currentPageUsersCount).to.be.equal(count);
-
-                done();
-            });
+            const usersProvided = await userService.findAll(queryPage, queryLimit);
+            expect(usersProvided).to.have.property('rows');
+            expect(usersProvided.rows.length).to.be.lessThanOrEqual(limit);
         });
 
-        it('retorna erro se a pagina definida pelo cliente for invalida', (done) => {
-            const limit = 2;
-            const page = 'paginaInvalida';
-            chai.request(app)
-            .get(`${BASE_URL}/users?page=${page}&limit=${limit}`)
-            .end(function (err, res) {
-                expect(res).to.have.status(400);
-                expect(res.body.message).to.be.equal('Bad Request: Invalid query page value: paginaInvalida');
-                done();
-            });
+        it('retorna erro se a pagina definida pelo cliente for invalida', async  () => {
+            const queryLimit = '2';
+            const queryPage = 'paginaInvalida';
+            try {
+                await userService.findAll(queryPage, queryLimit);
+            } catch (err) {
+                expect(err).to.have.property('type');
+                expect(err.type).to.be.equal('InvalidPageValueError');
+            }
         });
 
-        it('retorna erro se o limite definido pelo cliente for invalido', (done) => {
-            const limit = 'limiteInvalido';
-            const page = 1;
-            chai.request(app)
-            .get(`${BASE_URL}/users?page=${page}&limit=${limit}`)
-            .end(function (err, res) {
-                expect(res).to.have.status(400);
-                expect(res.body.message).to.be.equal('Bad Request: Invalid query limit value: limiteInvalido');
-                done();
-            });
+        it('retorna erro se o limite definido pelo cliente for invalido', async  () => {
+            const queryLimit = 'limite invalido';
+            const queryPage = '1';
+            try {
+                await userService.findAll(queryPage, queryLimit);
+            } catch (err) {
+                expect(err).to.have.property('type');
+                expect(err.type).to.be.equal('InvalidPaginationLimitValueError');
+            }
         });
 
         
@@ -177,167 +161,117 @@ describe('Testes unitários para service',  () => {
             return await loadDB();
         });
 
-        it('atualiza todos os campos do usuário', (done) => {
-            const userToUpdate = { nome:"fernando", email:"fernando@devoz.com.br", idade:31 };
-            chai.request(app)
-            .put(`${BASE_URL}/users/${userToUpdate['nome']}`)
-            .send(userToUpdate)
-            .end( (err, res) => {
-                expect(err).to.be.null;
-                expect(res).to.have.status(204)
-                done();
-            });
+        it('atualiza todos os campos do usuário', async  () => {
+            const userToUpdate = { nome:'fernando', email:'fernando@devoz.com.br', idade:31 };
+            const result = await userService.updateByName(userToUpdate.nome, userToUpdate);
+            expect(result).to.be.equal('success');
         });
-        it('atualiza o campo idade do usuário', (done) => {
-            const nome = "fernando";
-            const fieldPatch = {op: "replace", path: `/idade`, value: 45};
-            chai.request(app)
-            .patch(`${BASE_URL}/users/${nome}`)
-            .send(fieldPatch)
-            .end( (err, res) => {
-                expect(err).to.be.null;
-                expect(res).to.have.status(204)
-                done();
-            });
+        it('atualiza o campo idade do usuário', async  () => {
+            const nome = 'fernando';
+            const field = 'idade';
+            const value = 45;
+            const result = await userService.replaceField(nome, field, value);
+            expect(result).to.be.equal('success');
         });
-        it('atualiza o campo email do usuário', (done) => {
-            const nome = "fernando";
-            const fieldPatch = {op: "replace", path: `/email`, value: "fernando@mail.com"};
-            chai.request(app)
-            .patch(`${BASE_URL}/users/${nome}`)
-            .send(fieldPatch)
-            .end( (err, res) => {
-                expect(err).to.be.null;
-                expect(res).to.have.status(204)
-                done();
-            });
+        it('atualiza o campo email do usuário', async  () => {
+            const nome = 'fernando';
+            const field = 'email';
+            const value = 'fernando@mail.com';
+            const result = await userService.replaceField(nome, field, value);
+            expect(result).to.be.equal('success');
         });
-        it('retorna status 400 para pedidos inválidos no PUT', (done) => {
-            const userToUpdate = { nome:"fernando" };
-            chai.request(app)
-            .put(`${BASE_URL}/users/${userToUpdate['nome']}`)
-            .send({nome: "raupp"})
-            .end(function (err, res) {
-                expect(res).to.have.status(400);
-                done();
-            });
-        });
-        it('retorna campos invalidos para pedidos inválidos no PUT', (done) => {
-            const userToUpdate = { nome:"fernando" };
-            chai.request(app)
-            .put(`${BASE_URL}/users/${userToUpdate['nome']}`)
-            .send({nome: "raupp"})
-            .end(function (err, res) {
-                expect(res.body.message).to.be.equal('Bad Request: Invalid fields:\n' +
-                'notNull Violation: user.email cannot be null,' +
-                '\nnotNull Violation: user.idade cannot be null');
-                done();
-            });
-        });
-        it('retorna erro para operações inválidas no PATCH', (done) => {
-            const nome = "fernando";
-            const fieldPatch = {op: "rep", path: `/email`, value: "fernando@mail.com"};
-            chai.request(app)
-            .patch(`${BASE_URL}/users/${nome}`)
-            .send(fieldPatch)
-            .end(function (err, res) {
-                expect(res.status).to.be.equal(400);
-                expect(res.body.message).to.be.equal(`Bad Request: Invalid operation requested: ${fieldPatch.op}`);
-                done();
-            });
-        });
-        it('retorna erro para campos de modelo inválidos no PATCH', (done) => {
-            const nome = "fernando";
-            const fieldPatch = {op: "replace", path: `/ema`, value: "fernando@mail.com"};
-            chai.request(app)
-            .patch(`${BASE_URL}/users/${nome}`)
-            .send(fieldPatch)
-            .end(function (err, res) {
-                expect(res.status).to.be.equal(400);
-                expect(res.body.message).to.be.equal('Bad Request: Invalid field name:\nema is not a valid model field');
-                done();
-            });
+        it('retorna campos inválidos para pedidos inválidos de atualização', async  () => {
+            const userToUpdate = { nome:'fernando' };
+            try {
+                await userService.updateByName(userToUpdate.nome, userToUpdate);
+            } catch (err) {
+                expect(err).to.haveOwnProperty('type');
+                expect(err.type).to.be.equal('InvalidModelFieldsError');
+                expect(err).to.haveOwnProperty('invalidFields');
+                expect(err.invalidFields.length).to.be.equal(2);
+            }
         });
 
         const tests = [
             { 
                 field: 'email', 
                 value: 'invalidEmail', 
-                expectedMessage: 'Bad Request: Invalid fields:\nValidation error: Validation is on email failed'
+                expectedType: 'InvalidModelFieldsError'
             },
             { 
                 field: 'idade', 
                 value: 16, 
-                expectedMessage: 'Bad Request: Invalid fields:\nValidation error: Validation min on idade failed'
+                expectedType: 'InvalidModelFieldsError'
             },
+            {
+                field: 'ida',
+                value: 45,
+                expectedType: 'InvalidModelFieldNameError'
+            }
         ]
 
-        tests.forEach( ({field, value, expectedMessage}) => {
-            it(`retorna erro para ${field} inválido no PATCH`, (done) => {
-                const nome = "fernando";
-                const fieldPatch = {op: "replace", path: `/${field}`, value: value};
-                chai.request(app)
-                .patch(`${BASE_URL}/users/${nome}`)
-                .send(fieldPatch)
-                .end(function (err, res) {
-                    expect(res.status).to.be.equal(400);
-                    expect(res.body.message).to.be.equal(expectedMessage);
-                    done();
-                });
+        tests.forEach( ({field, value, expectedType}) => {
+            it(`retorna erro para ${field} inválido usando replaceField`, async  () => {
+                const nome = 'fernando';
+                try {
+                    await userService.replaceField(nome, field, value);
+                } catch (err) {
+                    expect(err).to.have.property('type');
+                    expect(err.type).to.be.equal(expectedType);
+                }
             });
         });
         
-        it('retorna erro caso o usuário não exista no PUT', (done) => {
-            chai.request(app)
-            .put(`${BASE_URL}/users/naoExiste`)
-            .end(function (err, res) {
-                expect(res).to.have.status(404);
-                expect(res.body.message).to.be.equal('Not Found: User with WHERE name=\'naoExiste\' not found');
-                done();
-            });
+        it('retorna erro caso o usuário não exista usando updateByName', async  () => {
+            const nome = 'naoExiste';
+            const email = 'naoExiste@mail.com';
+            const idade = 40;
+            const userToUpdate = { nome, idade,  email}
+                try {
+                    await userService.updateByName(nome, userToUpdate);
+                } catch (err) {
+                    expect(err).to.have.property('type');
+                    expect(err.type).to.be.equal('UserNotFoundError');
+                }
         });
-        it('retorna erro caso o usuário não exista no PATCH', (done) => {
-            const fieldPatch = {op: "replace", path: `/email`, value: "fernando@mail.com"};
-            chai.request(app)
-            .patch(`${BASE_URL}/users/naoExiste`)
-            .send(fieldPatch)
-            .end(function (err, res) {
-                expect(res).to.have.status(404);
-                expect(res.body.message).to.be.equal('Not Found: User with WHERE name=\'naoExiste\' not found');
-                done();
-            });
+        it('retorna erro caso o usuário não exista usando replaceField', async  () => {
+            const nome = 'naoExiste';
+            const field = 'email';
+            const value = 'fernando@mail.com';
+            try {
+                await userService.replaceField(nome, field, value);
+            } catch (err) {
+                expect(err).to.have.property('type');
+                expect(err.type).to.be.equal('UserNotFoundError');
+            }
         });
     });
 
     describe('Testes de exclusão de usuários do repositório', () => {
         before( async () => await loadDB() );
         after( async () => await cleanup() );
-        it('deveria excluir o usuario raupp', function (done) {
-            chai.request(app)
-            .delete(`${BASE_URL}/users/raupp`)
-            .end(function (err, res) {
-                expect(err).to.be.null;
-                expect(res).to.have.status(204);
-                done();
-            });
+        it('deveria excluir o usuario raupp', async function () {
+            const result = await userService.deleteByName('raupp'); 
         });
     
-        it('o usuario raupp não deve existir mais no sistema', function (done) {
-            chai.request(app)
-            .get(`${BASE_URL}/users/raupp`)
-            .end(function (err, res) {
-                expect(res).to.have.status(404);
-                done();
-            });
+        it('o usuario raupp não deve existir mais no sistema', async function () {
+            const nome = 'raupp';
+            await userService.deleteByName(nome); 
+            try {
+                await userService.findOneByName(nome);
+            } catch (err) {
+                expect(err).to.have.property('type');
+                expect(err.type).to.be.equal('UserNotFoundError');
+            }
         });
-        it('retorna erro caso o usuário não exista', (done) => {
-            chai.request(app)
-            .delete(`${BASE_URL}/users/naoExiste`)
-            .end(function (err, res) {
-                expect(res).to.have.status(404);
-                expect(res.body.message).to.be.equal('Not Found: User with WHERE name=\'naoExiste\' not found');
-                done();
-            });
+        it('retorna erro caso o usuário não exista', async  () => {
+            const nome = 'naoExiste';
+            try {
+                await userService.deleteByName(nome);
+            } catch (err) {
+                expect(err).to.have.property('type');
+                expect(err.type).to.be.equal('UserNotFoundError');
+            }
         });
     });
     
