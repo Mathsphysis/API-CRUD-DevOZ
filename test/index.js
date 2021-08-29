4a7701032ef3d420442917f9050f55f3aae8c86e
Maths Physis <smathsphysis@gmail.com>
2021-08-28 23:09:29 -0400

    test: Implementado testes para erros e testes dinamicos

--- test/index.js
+++ test/index.js
@@ -1,6 +1,5 @@
 //sample test
 //Para rodar os testes, use: npm test
-//PS: Os testes não estão completos e alguns podem comnter erros.
 
 // veja mais infos em:
 //https://mochajs.org/
@@ -15,36 +14,35 @@ const chaiHttp = require('chai-http');
 const chaiJson = require('chai-json-schema');
 const server = require('../src/index.js');
 const sequelize = require('../src/config/database');
+const userSchema = require('./userSchema');
+const User = require('../src/repository/sqlModel.js');
+const usersPredefined = require('./users.js');
 
 chai.use(chaiHttp);
 chai.use(chaiJson);
 
 const expect = chai.expect;
 
-before( async () => {
-    await sequelize.sync();
-});
+const BASE_URL = '/api/v1';
 
-//Define o minimo de campos que o usuário deve ter. Geralmente deve ser colocado em um arquivo separado
-const userSchema = {
-    title: "Schema do Usuario, define como é o usuario, linha 24 do teste",
-    type: "object",
-    required: ['nome', 'email', 'idade'],
-    properties: {
-        nome: {
-            type: 'string'
-        },
-        email: {
-            type: 'string'
-        },
-        idade: {
-            type: 'number',
-            minimum: 18
-        }
-    }
+const raupp = { nome: "raupp", email: "jose.raupp@devoz.com.br", idade: 35 };
+
+const loadDB = async function () {
+    const users = [ ...usersPredefined, raupp];
+    users.forEach( async (user) => {
+        return await chai.request(app)
+        .post(`${BASE_URL}/users`)
+        .send({ ...user });
+    });
+};
+
+const cleanup = async function () {
+    return await User.destroy({ truncate: true });
 }
 
-const BASE_URL = '/api/v1';
+before( async () => {
+    await sequelize.sync();  
+});
 
 //Inicio dos testes
 
@@ -76,10 +74,12 @@ describe('Testes da aplicaçao',  () => {
     });
 
     describe('Testes de criação de usuário', () => {
+        after( async () => await cleanup() );
+        
         it('deveria criar o usuario raupp', function (done) {
             chai.request(app)
             .post(`${BASE_URL}/users`)
-            .send({nome: "raupp", email: "jose.raupp@devoz.com.br", idade: 35})
+            .send(raupp)
             .end(function (err, res) {
                 expect(err).to.be.null;
                 expect(res).to.have.status(201);
@@ -88,33 +88,7 @@ describe('Testes da aplicaçao',  () => {
         });
         
         it('cria usuários predefinidos', function (done) {
-            const users = [
-                {
-                    nome: "marcela",
-                    email: "marcela@email.com",
-                    idade: 28
-                },
-                {
-                    nome: "fernando",
-                    email: "fernando@gmail.com",
-                    idade: 30
-                }, 
-                {
-                    nome: "ricardo",
-                    email: "ricardo@email.com",
-                    idade: 19
-                },
-                {
-                    nome: "gabriela",
-                    email: "gabriela@gmail.com",
-                    idade: 23
-                },
-                {
-                    nome: "isabela",
-                    email: "isabela@email.com",
-                    idade: 25
-                }
-            ];
+            const users = [ ...usersPredefined ];
             users.forEach((user) => {
                 chai.request(app)
                 .post(`${BASE_URL}/users`)
@@ -126,9 +100,32 @@ describe('Testes da aplicaçao',  () => {
             });
             done();
         });
+        it('retorna status 400 para pedidos inválidos', (done) => {
+            chai.request(app)
+            .post(`${BASE_URL}/users`)
+            .send({nome: "raupp"})
+            .end(function (err, res) {
+                expect(res).to.have.status(400);
+                done();
+            });
+        });
+        it('retorna campos invalidos para pedidos inválidos', (done) => {
+            chai.request(app)
+            .post(`${BASE_URL}/users`)
+            .send({nome: "raupp"})
+            .end(function (err, res) {
+                expect(res.error.text).to.be.equal('Bad request: Bad request: Invalid fields:\n' +
+                'notNull Violation: user.email cannot be null,' +
+                '\nnotNull Violation: user.idade cannot be null');
+                done();
+            });
+        });
+
     });
 
     describe('Testes de leitura do repositório de usuários', () => {
+        before( async () => await loadDB() );
+        after( async () => await cleanup() );
         it('deveria ser uma lista com pelo menos 5 usuarios', function (done) {
             chai.request(app)
             .get(`${BASE_URL}/users`)
@@ -143,7 +140,7 @@ describe('Testes da aplicaçao',  () => {
             chai.request(app)
             .get(`${BASE_URL}/users/naoExiste`)
             .end(function (err, res) {
-                expect(res.error.text).to.be.equal('User not found'); 
+                expect(res.error.text).to.be.equal('User with WHERE name=\'naoExiste\' not found'); 
                 expect(res).to.have.status(404);
                 done();
             });
@@ -165,6 +162,22 @@ describe('Testes da aplicaçao',  () => {
 
 
     describe('Testes de atualização do repositório de usuários', () => {
+        before( async () => await loadDB() );
+        after( async () => await cleanup() );
+
+        const tests = [
+            { 
+                field: 'email', 
+                value: 'invalidEmail', 
+                expectedMessage: 'Bad request: Bad request: Invalid fields:\nValidation error: Validation is on email failed'
+            },
+            { 
+                field: 'idade', 
+                value: 16, 
+                expectedMessage: 'Bad request: Bad request: Invalid fields:\nValidation error: Validation min on idade failed'
+            },
+        ]
+
         it('atualiza todos os campos do usuário', (done) => {
             const userToUpdate = { nome:"fernando", email:"fernando@devoz.com.br", idade:31 };
             chai.request(app)
@@ -200,9 +213,93 @@ describe('Testes da aplicaçao',  () => {
             });
             done();
         });
+        it('retorna status 400 para pedidos inválidos no PUT', (done) => {
+            const userToUpdate = { nome:"fernando" };
+            chai.request(app)
+            .put(`${BASE_URL}/users/${userToUpdate['nome']}`)
+            .send({nome: "raupp"})
+            .end(function (err, res) {
+                expect(res).to.have.status(400);
+                done();
+            });
+        });
+        it('retorna campos invalidos para pedidos inválidos no PUT', (done) => {
+            const userToUpdate = { nome:"fernando" };
+            chai.request(app)
+            .put(`${BASE_URL}/users/${userToUpdate['nome']}`)
+            .send({nome: "raupp"})
+            .end(function (err, res) {
+                expect(res.error.text).to.be.equal('Bad request: Bad request: Invalid fields:\n' +
+                'notNull Violation: user.email cannot be null,' +
+                '\nnotNull Violation: user.idade cannot be null');
+                done();
+            });
+        });
+        it('retorna erro para operações inválidas no PATCH', (done) => {
+            const nome = "fernando";
+            const fieldPatch = {op: "rep", path: `/email`, value: "fernando@mail.com"};
+            chai.request(app)
+            .patch(`${BASE_URL}/users/${nome}`)
+            .send(fieldPatch)
+            .end(function (err, res) {
+                expect(res.status).to.be.equal(400);
+                expect(res.error.text).to.be.equal(`Invalid operation requested: ${fieldPatch.op}`);
+                done();
+            });
+        });
+        it('retorna erro para campos de modelo inválidos no PATCH', (done) => {
+            const nome = "fernando";
+            const fieldPatch = {op: "replace", path: `/ema`, value: "fernando@mail.com"};
+            chai.request(app)
+            .patch(`${BASE_URL}/users/${nome}`)
+            .send(fieldPatch)
+            .end(function (err, res) {
+                expect(res.status).to.be.equal(400);
+                expect(res.error.text).to.be.equal('Bad request: Bad request: Invalid field name:\nema is not a valid model field');
+                done();
+            });
+        });
+
+        tests.forEach( ({field, value, expectedMessage}) => {
+            it(`retorna erro para ${field} inválido no PATCH`, (done) => {
+                const nome = "fernando";
+                const fieldPatch = {op: "replace", path: `/${field}`, value: value};
+                chai.request(app)
+                .patch(`${BASE_URL}/users/${nome}`)
+                .send(fieldPatch)
+                .end(function (err, res) {
+                    expect(res.status).to.be.equal(400);
+                    expect(res.error.text).to.be.equal(expectedMessage);
+                    done();
+                });
+            });
+        });
+        
+        it('retorna erro caso o usuário não exista no PUT', (done) => {
+            chai.request(app)
+            .put(`${BASE_URL}/users/naoExiste`)
+            .end(function (err, res) {
+                expect(res).to.have.status(404);
+                expect(res.error.text).to.be.equal('User with WHERE name=\'naoExiste\' not found');
+                done();
+            });
+        });
+        it('retorna erro caso o usuário não exista no PATCH', (done) => {
+            const fieldPatch = {op: "replace", path: `/email`, value: "fernando@mail.com"};
+            chai.request(app)
+            .patch(`${BASE_URL}/users/naoExiste`)
+            .send(fieldPatch)
+            .end(function (err, res) {
+                expect(res).to.have.status(404);
+                expect(res.error.text).to.be.equal('User with WHERE name=\'naoExiste\' not found');
+                done();
+            });
+        });
     });
 
     describe('Testes de exclusão de usuários do repositório', () => {
+        before( async () => await loadDB() );
+        after( async () => await cleanup() );
         it('deveria excluir o usuario raupp', function (done) {
             chai.request(app)
             .delete(`${BASE_URL}/users/raupp`)
@@ -217,8 +314,16 @@ describe('Testes da aplicaçao',  () => {
             chai.request(app)
             .get(`${BASE_URL}/users/raupp`)
             .end(function (err, res) {
-                expect(err).to.be.null;
                 expect(res).to.have.status(404);
+                done();
+            });
+        });
+        it('retorna erro caso o usuário não exista', (done) => {
+            chai.request(app)
+            .delete(`${BASE_URL}/users/naoExiste`)
+            .end(function (err, res) {
+                expect(res).to.have.status(404);
+                expect(res.error.text).to.be.equal('User with WHERE name=\'naoExiste\' not found');
                 done();
             });
         });
---
+++ test/userSchema.js
@@ -0,0 +1,19 @@
+const userSchema = {
+    title: "Schema do Usuario, define como é o usuario",
+    type: "object",
+    required: ['nome', 'email', 'idade'],
+    properties: {
+        nome: {
+            type: 'string'
+        },
+        email: {
+            type: 'string'
+        },
+        idade: {
+            type: 'number',
+            minimum: 18
+        }
+    }
+}
+
+module.exports = userSchema;
